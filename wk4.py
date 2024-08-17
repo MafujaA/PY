8cdea4f574c800ab1d240f04da702be9d98d79c7
Mafuja Akhtar <167004066+MafujaA@users.noreply.github.com>
2024-08-18 00:08:10 +0930

    initial commit

---
+++ wk4.py
@@ -0,0 +1,25 @@
+first = [1,2,3,4,5]
+second = list(range(1, 6))
+
+print(first)
+print(second)
+print(first == second) # False
+second = second + [4, 5]
+print = (first == second) 
+
+x = [101, 215, 30, 40, 0, 54, 0]
+target = 0
+print(x.index(target))
+
+print [x.index(target)]
+
+example = [1, 2]
+print(example)
+def square(a):
+
+    print (a * a)
+    
+    square(3)
+
+
+
